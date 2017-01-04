db12dcb9b8d0376c490cbeac0458ba20ead39818
LambdaXymox <lambda.xymox@gmail.com>
2017-01-04 12:48:01 -0600

    Write first boot loader.

---
+++ src/arch/x86_64/multiboot_header.asm
@@ -0,0 +1,16 @@
+section .multiboot_header
+header_start:
+    dd 0xe85250d6                ; magic number
+    dd 0                         ; architecture (0 for protected mode i386, 4 for MIPS)
+    dd header_end - header_start ; header length
+    ; checksum
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+    ; The multiboot tags are optional. See section 3.1.1 of the
+    ; multiboot specification.
+
+    ; end tag
+    dw 0    ; type
+    dw 0    ; flags
+    dd 8    ; size
+header_end:
