cf7da867a34ffe49779692345c978f67b7265df9
LambdaXymox <lambda.xymox@gmail.com>
2017-01-05 22:34:12 -0600

    Add procedure to boot.asm to check for multiboot.

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -4,9 +4,9 @@ section .text
 bits 32
 start:
     call check_protected_mode
-    cmp eax, 0x00
-    je .ok
     cmp eax, 0x01
+    je .ok
+    cmp eax, 0x00
     je .not_ok
     jmp .error
 .ok:
@@ -22,15 +22,22 @@ start:
     mov dword [0xb8000], 0x2f522f45
     hlt
 
+; Detect that the intel CPU is in protected mode. 
 check_protected_mode:
-    ; push eax
     mov eax, cr0
     and eax, 0x01
     cmp eax, 0x01
     jne .not_protected_mode
-    mov eax, 0x00
-    ; pop eax
-    ret
-.not_protected_mode:
     mov eax, 0x01
     ret
+.not_protected_mode:
+    mov eax, 0x00
+    ret
+
+check_multiboot:
+    cmp eax, 0x36d76289
+    jne .no_multiboot
+    ret
+.no_multiboot:
+    mov al, "0"
+    jmp error
