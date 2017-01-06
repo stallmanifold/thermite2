2558fa1f8cb0cdaa50f02623c9ee88aaf3188240
LambdaXymox <lambda.xymox@gmail.com>
2017-01-06 02:31:58 -0600

    Add code for checking cpuid instruction availability.

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -56,17 +56,10 @@ check_multiboot:
     mov al, "0"
     jmp error
 
-; Prints `ERR: ` and the given error code to screen and hangs.
-; parameter: error code (in ascii) in al
-error:
-    mov dword [VGA_BUFFER], 0x4f524f45
-    mov dword [VGA_BUFFER+0x04], 0x4f3a4f52
-    mov dword [VGA_BUFFER+0x08], 0x4f204f20
-    mov byte  [VGA_BUFFER+0x0a], al
-    hlt
 
 vga_clear_buffer:
     
+
 ; Print a string to the VGA Buffer.
 vga_print_string:
     mov ecx, VGA_BUFFER
@@ -82,6 +75,54 @@ vga_print_string:
     jmp .loop
 .done:
     ret
+
+
+check_cpuid:
+    ; Check if CPUID is supported by attempting to flip the ID bit (bit 21)
+    ; in the FLAGS register. If we can flip it, CPUID is available.
+
+    ; Copy FLAGS in to EAX via stack
+    pushfd
+    pop eax
+
+    ; Copy to ECX as well for comparing later on
+    mov ecx, eax
+
+    ; Flip the ID bit
+    xor eax, 1 << 21
+
+    ; Copy EAX to FLAGS via the stack
+    push eax
+    popfd
+
+    ; Copy FLAGS back to EAX (with the flipped bit if CPUID is supported)
+    pushfd
+    pop eax
+
+    ; Restore FLAGS from the old version stored in ECX (i.e. flipping the
+    ; ID bit back if it was ever flipped).
+    push ecx
+    popfd
+
+    ; Compare EAX and ECX. If they are equal then that means the bit
+    ; wasn't flipped, and CPUID isn't supported.
+    cmp eax, ecx
+    je .no_cpuid
+    ret
+.no_cpuid:
+    mov al, "1"
+    jmp error
+
+
+; Prints `ERR: ` and the given error code to screen and hangs.
+; parameter: error code (in ascii) in al
+error:
+    mov dword [VGA_BUFFER], 0x4f524f45
+    mov dword [VGA_BUFFER+0x04], 0x4f3a4f52
+    mov dword [VGA_BUFFER+0x08], 0x4f204f20
+    mov byte  [VGA_BUFFER+0x0a], al
+    hlt
+
 
 section .bss
 align 4096
